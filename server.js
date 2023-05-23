81f34021d074f3ed1c06f39d5f44e955176aed39
Emtiaz-pias <emtiazpias383@gmail.com>
2023-05-23 17:26:07 -0400

    users sign in post  api created

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // db connections
-const uri = "mongodb+srv://pias:aT0Bs1OmHxPFPgpc@cluster0.7yyq5li.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://pias1:jwu3MQasnPn2NA2P@cluster0.7yyq5li.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -19,13 +19,40 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
-    const usersCollectin= client.db("laundryApp").collection("users")
+    const usersCollection= client.db("laundryApp").collection("users")
+    const ordersCollection = client.db("laundryApp").collection("orders")
     
-
+    // my apis 
+    console.log("db cnnected")
+    //all post api 
+app.post("/signUp", (req, res) => {
+  usersCollection.insertOne(req.body, (err, result) => {
+    if (err) {
+      console.error('Error inserting document:', err);
+      res.status(500).json({ error: 'Please try again' });
+    } else {
+      console.log('Document inserted:', result.insertedId);
+      res.status(201).json({ message: 'Congratulation signed in successfully' });
+    }
+})
+console.log(req.body);
+})
+app.post("/order", (req, res) => {
+  usersCollection.insertOne(req.body, (err, result) => {
+    if (err) {
+      console.error('Error inserting document:', err);
+      res.status(500).json({ error: 'Please try again' });
+    } else {
+      console.log('Document inserted:', result.insertedId);
+      res.status(201).json({ message: 'Congratulation your has placed successfully' });
+    }
+})
+console.log(req.body);
+})
   } catch {
    console.log(err)
   }
-  app.listen(3000, () => {
+  app.listen(5000, () => {
     console.log("app is alive");})
   
 }
