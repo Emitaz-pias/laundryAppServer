14375e099173d7e730bc5eef96f90d1a30938a83
Emtiaz-pias <emtiazpias383@gmail.com>
2023-05-28 01:32:56 -0700

    sign in api done

--- server.js
+++ server.js
@@ -19,24 +19,57 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
-    const usersCollection= client.db("laundryApp").collection("users")
-    const ordersCollection = client.db("laundryApp").collection("orders")
+    const usersCollection= client.db("laundryApp").collection("users");
+    
+    // get all users
+    app.get("/user",(req,res)=>{
+      usersCollection.find({}).toArray((err, result) => {
+        console.log(result,err)
+        res.send(result)
+      })
+     
+    })
+
+    app.post('/login', (req, res) => {
+      const { email, password } = req.body; // Assuming the request body contains username and password fields
+    
+      // Perform server logic to check credentials
+      usersCollection.find({ "email":'emtiazpias22@gmail.com'}, (err, user) => {
+        if (err) {
+          console.error('Error finding user:', err);
+          res.status(500).json({ error: 'An error occurred' });
+        } else if (user) {
+          // User found, authentication successful
+          console.log('User authenticated:', user.username);
+          res.status(200).json({ message: 'Authentication successful' });
+        } else {
+          // User not found, authentication failed
+          console.log('Authentication failed');
+          res.status(401).json({ error: 'Authentication failed' });
+        }
+      });
+    });
     
     // my apis 
     console.log("db cnnected")
     //all post api 
+
+    // sign up user api
 app.post("/signUp", (req, res) => {
-  usersCollection.insertOne(req.body, (err, result) => {
+const user = req.body;
+  usersCollection.insertOne(user).then((result,err) => {
     if (err) {
-      console.error('Error inserting document:', err);
-      res.status(500).json({ error: 'Please try again' });
-    } else {
-      console.log('Document inserted:', result.insertedId);
-      res.status(201).json({ message: 'Congratulation signed in successfully' });
+      console.log(err,"err msg")
     }
+    else{
+console.log("n err")
+res.send(result.acknowledged);
+    }
+  
+  })
+
 })
-console.log(req.body);
-})
+//place order api
 app.post("/order", (req, res) => {
   usersCollection.insertOne(req.body, (err, result) => {
     if (err) {
@@ -49,6 +82,11 @@ app.post("/order", (req, res) => {
 })
 console.log(req.body);
 })
+
+//// find apis ////
+
+
+
   } catch {
    console.log(err)
   }
